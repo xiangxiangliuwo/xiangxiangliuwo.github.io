d20def34c62f85f59113281cb035d74ca1f2d111
xiangxiangliuwo <zzzzyguo@163.com>
2026-04-04 13:18:33 +0000

    Deploying to gh-pages from @ xiangxiangliuwo/xiangxiangliuwo.github.io@8053074c8b754695bc31aa9f6d3ac3a76a7d6212 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A summary of my academic journey, research experiences and publications is shown in this page. You can access a more detailed version via the PDF button at the top right corner.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
